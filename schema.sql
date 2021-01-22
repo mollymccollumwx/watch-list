708983baef53e0b1d97e4f3178a18cd55054e08c
MBP16 <jonathanwatson@MBP16s-MacBook-Pro.local>
2021-01-22 12:56:15 -0500

    built out server

--- schema.sql
+++ schema.sql
@@ -8,7 +8,7 @@ CREATE DATABASE moviePlannerDB;
 USE moviePlannerDB;
 
 -- Create the table plans.
-CREATE TABLE plans (
+CREATE TABLE movies (
   id int NOT NULL AUTO_INCREMENT,
   movie varchar(255) NOT NULL,
   PRIMARY KEY (id)
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Mickey19",
+  database: "moviePlannerDB"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
